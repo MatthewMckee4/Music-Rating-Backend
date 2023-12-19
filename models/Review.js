25c51f2c4079c93af78eae3a8b747d0dc51fe5e8
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2023-12-19 15:09:35 +0000

    updated review schema

--- models/Review.js
+++ models/Review.js
@@ -3,13 +3,7 @@ const mongoose = require("mongoose");
 const reviewSchema = new mongoose.Schema({
   user_id: { type: String, required: true },
   album_id: { type: String, required: true },
-  rating: {
-    type: Number,
-    validate: {
-      validator: Number.isInteger,
-      message: "{VALUE} is not an integer value",
-    },
-  },
+  rating: { type: Number },
   text: { type: String },
 });
 
